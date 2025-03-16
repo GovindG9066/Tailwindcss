c980e2492d6848b087be8a276bb9962e8e77434f
Govind9066 <gadekargovind575@gmail.com>
2025-03-16 22:33:15 +0530

    adding the dark mode button

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 let  Object_container=document.querySelector(".object-container");
 
+let btn_cont=document.querySelector(".btn-cont");
+let btn_circle=document.querySelector(".btn-circle");
+
+function mode() {
+    btn_cont.addEventListener("click",()=>{
+        btn_circle.classList.toggle("translate-x-full");
+        console.log("click");
+        
+    })
+}
+mode();
