4a38d98e02e0e4d24df1868e944f0cc36918446f
Govind9066 <gadekargovind575@gmail.com>
2025-03-17 21:26:31 +0530

    adding the images and the some content

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ let  Object_container=document.querySelector(".object-container");
 let btn_cont=document.querySelector(".btn-cont");
 let btn_circle=document.querySelector(".btn-circle");
 
-function mode() {
-    btn_cont.addEventListener("click",()=>{
-        btn_circle.classList.toggle("translate-x-full");
-        console.log("click");
+// function mode() {
+//     btn_cont.addEventListener("click",()=>{
+//         btn_circle.classList.toggle("translate-x-full");
+//         console.log("click");
         
-    })
-}
-mode();
+//     })
+// }
+// mode();
+
+btn_cont.addEventListener("click",()=>{
+    btn_circle.classList.toggle("translate-x-full");
+    console.log("clicked");
+    
+
+})
