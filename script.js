2092c5a5775b69a43b95fb6ab9cb9cb628203fee
Govind9066 <gadekargovind575@gmail.com>
2025-03-31 20:06:53 +0530

    removing the unwanted code form the all files

--- script.js
+++ script.js
@@ -3,17 +3,9 @@ let Object_container = document.querySelector(".object-container");
 let btn_cont = document.querySelector(".btn-cont");
 let btn_circle = document.querySelector(".btn-circle");
 
-// function mode() {
-//     btn_cont.addEventListener("click",()=>{
-//         btn_circle.classList.toggle("translate-x-full");
-//         console.log("click");
 
-//     })
-// }
-// mode();
 
 btn_cont.addEventListener("click", () => {
-    // btn_circle.classList.toggle("translate-x-full");
     btn_circle.classList.toggle("move-btn");
     document.body.classList.add("dark");
     if (btn_circle.classList.contains("move-btn")) {
